4503758efa8a81af911e283659ea796bd628cce1
Nathan Tum <tumkipsang@gmail.com>
2024-12-24 22:02:48 -0600

    Solved all the problems

---
+++ Ch01_Solutions.py
@@ -0,0 +1,70 @@
+# ************************************#
+#  Author: Nathan T                   #
+#  Email: tumkipsang@gmail.com        #
+#  Chapter 1 Problems Solutions       #
+# ************************************#
+
+
+# Practice Questions
+# 1. Which of the following are operators, and which are values?
+
+    # * Operator
+    # 'hello' Value
+    # -88.8 Value
+    # - Operator
+    # / Operator
+    # + Operator
+    # 5 Value
+
+# 2. Which of the following is a variable, and which is a string?
+
+    # spam   variable
+    # 'spam' string
+
+# 3. Name three data types.
+    #  int
+    #  double
+    #  float
+    #  string
+
+# 4. What is an expression made up of? What do all expressions do?
+    # operators
+    # variables
+    # assignment
+
+# 5. This chapter introduced assignment statements, like spam = 10. 
+# What is the difference between an expression and a statement?
+    # statement is a group of expression
+    # expression is two sided with an assignment operator
+
+# 6. What does the variable bacon contain after the following code runs?
+
+    # bacon = 20
+    # bacon + 1
+
+    # It creates a variable named bacon and assigns a value of 20 
+    # and finals increaments it by 1
+
+# 7. What should the following two expressions evaluate to?
+
+    # 'spam' + 'spamspam'  This evaluates to  'spamspamspam'
+    # 'spam' * 3 This evaluates to 'spamspamspam''
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+    # egg is a string and satisfies the requirements for a variable name
+    # 
+
+# 9. What three functions can be used to get the integer, 
+# floating-point number, or string version of a value?
+
+    # str()
+    # int()
+    # float()
+
+
+# 10. Why does this expression cause an error? How can you fix it?
+
+# 'I have eaten ' + 99 + ' burritos.'
+
+    # Syntax error. Cannot add a string to an integer
+    # Fix: Change 99 to a string e.g 'I have eaten ' + '99' + ' burritos.'
